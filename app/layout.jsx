49b174212411d897cd3e0f80ad443a61a0b6a051
mohammad kabiri <codeversesite@gmail.com>
2024-08-03 11:18:07 +0330

    config tailwind

---
+++ app/layout.jsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+
+export const metadata = {
+  title: " دانشگاه علم و فرهنگ",
+  description: "University of Science and Culture",
+  icons: {
+    icon: "/image/logo/logo.png",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fa" dir="rtl">
+      <body className="font-Dana bg-gray-100 dark:bg-zinc-800">
+        {children}
+      </body>
+    </html>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,53 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  darkMode: "class",
+  content: ["./public/*.{html,js}", "./src/js/*.{html,js}"],
   theme: {
     extend: {
+      borderRadius: {
+        "4xl": "2rem",
+      },
+      fontFamily: {
+        "Dana": "Dana",
+        "DanaMedium": "Dana Medium",
+        "DanaBold": "Dana DemiBold",
+        "MorabbaLight": "Morabba Light",
+        "MorabbaMedium": "Morabba Medium",
+        "MorabbaBold": "Morabba Bold",
+      },
+      spacing: {
+        "25": "6.25rem",
+        "30": "7.5rem",
+        "50": "12.5rem",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          lg: "0.625rem"
+        }
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "home-mobile": "url(../utils/image/background/business.jpg)",
+        "home-desktop": "url(../utils/image/background/business.jpg)",
       },
     },
+    screens: {
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px'
+    },
   },
-  plugins: [],
-};
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant('child', '&> *');
+      addVariant('child-hover', '&> *:hover');
+    })
+  ],
+}
+
