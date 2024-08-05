ad08d0ecbc9ae10e6d0e5e089062a61838bb09b8
mohammad kabiri <codeversesite@gmail.com>
2024-08-05 09:56:20 +0330

    edit main

--- app/page.jsx
+++ app/page.jsx
@@ -11,9 +11,9 @@ export default function Home() {
   return (
     <main className="">
       <SectionHome />
-      <SectionTeach />
-      <SectionCarousel />
       <SectionContents />
+      <SectionCarousel />
+      <SectionTeach />
       <SectionSpecialOffer />
       <SectionCourses />
       <SectionAbout />
